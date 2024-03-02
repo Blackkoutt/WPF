c9d6c8a9eb29d248cb22e82697a514aaf2aa00c1
Mateusz Strapczuk <mateusz.strapczuk@gmail.com>
2024-03-02 01:20:21 +0100

    Initial commit ex1

---
+++ Ex1/Ex1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Ex1.Models;
+using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Ex1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DrawingMachine drawingMachine;
+        public MainWindow()
+        {
+            drawingMachine = new DrawingMachine();
+            InitializeComponent();
+        }
+
+        private void TakeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                string randomTicketValue = drawingMachine.GetRandomTicket();
+                MainOutputLabel.Content = $"Drawn value: {randomTicketValue}";
+                MainOutputLabel.Foreground = Brushes.Green;
+                OutputLabel.Text = $"Remaining tickets: {string.Join(",", drawingMachine.Tickets)}";
+            }
+            catch (Exception ex) 
+            {
+                MainOutputLabel.Content = ex.Message;
+                MainOutputLabel.Foreground = Brushes.Red;
+            }       
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string ticketValue = TicketNameInput.Text;
+            try
+            {
+                drawingMachine.AddTicket(ticketValue);
+                MainOutputLabel.Content = $"Ticket {ticketValue} added succesfully!";
+                MainOutputLabel.Foreground = Brushes.Green;
+                OutputLabel.Text = $"Tickets in drawing machine: {string.Join(",", drawingMachine.Tickets)}";
+            }
+            catch(Exception ex)
+            {
+                MainOutputLabel.Content = ex.Message;
+                MainOutputLabel.Foreground = Brushes.Red;
+            }           
+        }
+    }
+}
---
+++ Ex1/Ex1/Models/DrawingMachine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex1.Models
+{
+    public class DrawingMachine
+    {
+        private List<string> tickets;
+        public DrawingMachine()
+        {
+            tickets = new List<string>();
+            AddTicket("abc");
+            AddTicket("def");
+        }
+        public void AddTicket (string ticket)
+        {
+            if(ticket.Trim() == "")
+            {
+                throw new Exception("Ticket cannot contains only spaces or be empty!");           
+            }
+            else if (tickets.Contains(ticket))
+            {
+                throw new Exception("Such ticket already exists in drawing machine!");
+            }
+            else
+            {
+                tickets.Add(ticket);
+            }
+            
+        }
+        public string GetRandomTicket()
+        {
+            if (IsTicketsAvailable())
+            {
+                Random rnd = new Random();
+                int randomIndex = rnd.Next(0, tickets.Count);
+                string ticket = tickets[randomIndex];
+                tickets.Remove(ticket);
+                return ticket;
+            }
+            throw new Exception("List Is Empty !");
+        }
+        public bool IsTicketsAvailable()
+        {
+            return tickets.Any();
+        }
+        public List<string> Tickets
+        {
+            get { return tickets; }
+        }
+    }
+}
