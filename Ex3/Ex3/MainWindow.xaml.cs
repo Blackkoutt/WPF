69c89159ff904ea944b4cf9a367a890f51ffb14b
Mateusz Strapczuk <mateusz.strapczuk@gmail.com>
2024-04-01 22:46:44 +0200

    Initial commit Ex3

---
+++ Ex3/Ex3/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using Ex3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Order order;
+        public MainWindow()
+        {
+            order = new Order();
+            InitializeComponent();
+            UpdateOrderSummary();
+        }
+        private void LoadLabelTextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textbox = (TextBox)sender;    
+            try
+            {
+                order.ItemsQuantity = int.Parse(textbox.Text);
+                UpdateOrderSummary();
+
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int newValue = (int)e.NewValue;
+            double formatCost = 0.20;
+            for (int i = 0; i<newValue; i++)
+            {
+                formatCost *= 2.5;
+            }
+            formatCost = Math.Round(formatCost, 2);
+            order.Format = $"A{5 - newValue}";
+            order.FormatCost = formatCost;
+
+            if (formatCost < 1)
+            {
+                formatLabel.Content = $"A{5 - newValue} - cena {(int)(formatCost * 100)}gr/szt.";
+            }
+            else
+            {
+                formatLabel.Content = $"A{5 - newValue} - cena {formatCost}zł/szt.";
+            }
+
+            // podlicz cala cene 
+            UpdateOrderSummary();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // do nothing
+        }
+
+        private void Grammage_RadioButton_Change(object sender, RoutedEventArgs e)
+        {
+           foreach(RadioButton radioButton in grammageStackPanel.Children)
+           {
+                if (radioButton.IsChecked == true && radioButton.Content!=null)
+                {
+                    string radioButtonTag = radioButton.Tag.ToString();
+                    if(radioButtonTag == "one_and_half")
+                    {
+                        radioButtonTag = "1,5";
+                    }
+                        
+                    double priceMultiplier = double.Parse(radioButtonTag);
+                    order.Grammage = radioButton.Content.ToString().Split("-")[0].Trim();
+                    if(priceMultiplier == 0)
+                    {
+                        order.GrammagePrice = 0;
+                    }
+                    else
+                    {
+                        order.GrammagePrice = Math.Round(order.FormatCost * priceMultiplier, 2);
+                    }    
+
+                    // podlicz cala cene 
+                    UpdateOrderSummary();
+
+                    break;
+                }
+            }
+        }
+        private void ColorfullPaperChecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            if (checkBox != null && checkBox.IsChecked == true) 
+            {
+                order.ColorfulPaperCost = order.FormatCost / 2;
+            }
+            else
+            {
+                order.ColorfulPaperCost = 0;
+            }
+            
+            UpdateOrderSummary();
+        }
+        
+        private void DeadlineOfRealizationRadioButtonChecked(object sender, RoutedEventArgs e)
+        { 
+            foreach(RadioButton radioButton in deadlineOfRealizationStackPanel.Children)
+            {
+                if(radioButton.IsChecked == true && radioButton.Content != null)
+                {
+                    order.DeadlineOfRealizationCost = double.Parse(radioButton.Tag.ToString());
+                    order.DeadlineOfRealization = radioButton.Content.ToString().Split("-")[0].Trim();
+                    UpdateOrderSummary();
+                    break;
+                }
+            }
+        }    
+        private void PrintOptionsCheckboxChecked(object sender, RoutedEventArgs e)
+        {
+            double defaultPrice = order.FormatCost;
+            double printOptionsCost = 0;
+            List<string> printOptions = new List<string>();
+            foreach (CheckBox checkbox in printOptionsStackPanel.Children)
+            {
+                if (checkbox.IsChecked == true && checkbox.Content!=null)
+                {
+                    int value;
+                    try
+                    {
+                        printOptions.Add(checkbox.Content.ToString().Split("-")[0].Trim());
+                        value = int.Parse(checkbox.Content.ToString().Split("+")[1].Split("%")[0]);
+                        printOptionsCost += (defaultPrice / 100) * value;
+                    }
+                    catch(Exception)
+                    {
+                        return;
+                    }
+                }
+            }
+            order.PrintOptions = printOptions;
+            order.PrintOptionsCost = printOptionsCost;
+            UpdateOrderSummary();
+        }
+
+        private void Button_OK_Clicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("Zamówienie zostało potwierdzone");
+            // reest form
+        }
+        private void Button_Cancel_Clicked(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UpdateOrderSummary()
+        {
+            if(orderSummaryTextBox != null)
+            {
+                orderSummaryTextBox.Text = order.GetOrderSummary();
+            }
+            
+        }
+
+
+        // rabat 1% za każde 100 egzamplarzy
+        // rabat max 10 %
+    }
+}
---
+++ Ex3/Ex3/Model/Order.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3.Model
+{
+    public class Order
+    {      
+        private string format;
+        private string grammage;
+        private int itemsQuantity;
+        private double formatCost;
+        private double grammagePrice;
+        private double colorfulPaperCost;
+        private double printOptionsCost;
+        private double deadlineOfRealizationCost;
+        private List<string> printOptions;
+        private string deadlineOfRealization;
+        private double priceBeforeDiscount;
+        private int discountPercentage;
+        private double priceAfterDiscount;
+
+        public Order()
+        {        
+            format = "A5";           
+            printOptions = new List<string>();
+            itemsQuantity = 0;
+            formatCost = 0.20;
+            grammagePrice = 0;
+            grammage = "80 g/m2";
+            //printOptionsCost = (formatCost / 100) * 30;
+           // printOptions.Add("druk w kolorze");
+            deadlineOfRealizationCost = 0;
+            deadlineOfRealization = "Standard";
+            colorfulPaperCost = 0;
+            
+
+            discountPercentage = 0;
+
+            priceBeforeDiscount = 0;
+            priceAfterDiscount = 0;
+        }
+        public string GetOrderSummary()
+        {
+            CalculatePrice();
+
+            return $"Przedmiot zamówienia: {itemsQuantity} szt., format {format}, {grammage}," +
+                    $"{(printOptions.Any() ? " " + string.Join(", ", printOptions) + "," : "")} {(colorfulPaperCost == 0 ? "" : "kolorowy papier, ")}" +
+                    $"{deadlineOfRealization}\n"+
+                    $"Cena przed rabatem: {priceBeforeDiscount} zł\n" +
+                    $"Naliczony rabat: {discountPercentage}% \n" +
+                    $"Cena po rabacie: {priceAfterDiscount} zł";
+        }
+        private void CalculateDiscount(double price)
+        {
+            int items_temp = itemsQuantity;
+            discountPercentage = 0;
+            while (items_temp >= 100)
+            {
+                discountPercentage += 1;
+                items_temp -= 100;
+            }
+            priceAfterDiscount = Math.Round(price - ((price/100) * discountPercentage),2);
+        }
+        private void CalculatePrice()
+        {
+            double helo = deadlineOfRealizationCost;
+
+            priceBeforeDiscount = Math.Round((itemsQuantity * (formatCost + grammagePrice + printOptionsCost
+                                                     + colorfulPaperCost)) + deadlineOfRealizationCost, 2);
+            int a = 1;
+            CalculateDiscount(priceBeforeDiscount);
+        }
+        public double DeadlineOfRealizationCost
+        {
+            get { return deadlineOfRealizationCost; }
+            set { deadlineOfRealizationCost = value; }
+        }
+        public string DeadlineOfRealization
+        {
+            get { return deadlineOfRealization; }
+            set { deadlineOfRealization = value; }
+        }
+        public double PrintOptionsCost
+        {
+            get { return printOptionsCost; }
+            set { printOptionsCost = value; }
+        }
+        public double ColorfulPaperCost
+        {
+            get { return colorfulPaperCost; }
+            set { colorfulPaperCost = value; }
+        }
+        public double GrammagePrice
+        {
+            get { return grammagePrice; } 
+            set { grammagePrice=value; } 
+        }
+        public int ItemsQuantity 
+        {
+            get { return itemsQuantity; } 
+            set { itemsQuantity = value; }
+        }
+        public string Format
+        {
+            get { return format; }
+            set { format = value; }
+        }
+        public string Grammage
+        {
+            get { return grammage; }
+            set { grammage = value; }
+        }
+        public List<string> PrintOptions
+        {
+            get { return printOptions; }
+            set { printOptions = value; }
+        }
+        public int DiscountPercentage
+        {
+            get { return discountPercentage; }
+            set { discountPercentage = value; }
+        }
+        public double FormatCost
+        {
+            get { return formatCost; }
+            set { formatCost = value; }
+        }
+        public double PriceBeforeDiscount
+        {
+            get { return priceBeforeDiscount; }
+            set { priceBeforeDiscount = value; }
+        }
+        public double PriceAfterDiscount
+        {
+            get { return priceAfterDiscount; }
+            set { priceAfterDiscount = value; }
+        }
+    }
+}
